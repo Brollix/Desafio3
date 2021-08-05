ac1bc6ec8b5f81b2cc5ad77096fca7d331d1f8a6
brollix <brolloagus@gmail.com>
2021-08-05 01:10:50 -0300

    commit

--- main.js
+++ main.js
@@ -2,9 +2,14 @@
 crear un algoritmo que repita un bloque de instrucciones. 
 En cada repetición es necesario efectuar una operación 
 o comparación para obtener una salida por alerta o consola. */
+
 alert('Te busco todos los años bisiestos hasta el año que me digas.');
 
 let año = parseInt(prompt('Ingresa un Número entero:'));
+
+while (isNaN(año)) {
+	año = parseInt(prompt('Incorrecto. Ingresa un Numero Entero:'));
+}
 
 let bisiesto = [];
 
